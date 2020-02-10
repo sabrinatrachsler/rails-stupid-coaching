9e5e5d6f09a1312d5bba53c7b2c2f5f336be3a8a
Sabrina Trachsler <sabrina.trachsler@hotmail.com>
2020-02-10 15:35:10 +0100

    exo 2, 1-3, not finished with 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  def ask
+  end
+
+  def answer
+    @answer = ANSWER.select do |id, answer|
+      if params[:question] == 'I am going to work'
+        puts answer[1]
+      elsif params[:question].match(/\w\?*/)
+        puts answer[2]
+      else
+        puts answer[3]
+
+    end
+  end
+
 end
+
+
+# If the message is I am going to work, the coach will answer Great!
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+# Otherwise she/he will answer I don't care, get dressed and go to work!
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/ask', to: 'pages#ask'
+  get 'pages/answer', to: 'pages#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#ask'
+  root to: 'pages#answer'
 end
